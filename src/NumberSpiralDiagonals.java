fbfa3a18aa6e83a79a01fd4daef349ce5da25389
Montana Ruth <montanalruth@gmail.com>
2015-06-13 07:34:23 -0500

    Optimizing NumberSpiralDiagonals

--- src/NumberSpiralDiagonals.java
+++ src/NumberSpiralDiagonals.java
@@ -9,20 +9,15 @@ public class NumberSpiralDiagonals
 {
   public static void main(String[] args)
   {
-    long lastNum = 1;
     long sum = 1;
-    long step = 2;
-    long max = 1001 * 1001;
 
-    while (lastNum < max)
+    for (int i = 3; i <= 1001; i += 2)
       {
-        for (int i = 0; i < 4; i++)
-          {
-            lastNum += step;
-            sum += lastNum;
-          }
-        step += 2;
+        sum += ((4 * (i * i)) - (6 * i) + 6);
       }
+
     System.out.println(sum);
+
   }
+
 }
